15997f38b9d8d64ec3935ccd762799d2765ce32c
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-06-26 14:34:55 +1000

    settings

--- backend/seek_backend/seek_backend/settings.py
+++ backend/seek_backend/seek_backend/settings.py
@@ -109,22 +109,22 @@ WSGI_APPLICATION = 'seek_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-# DATABASES = {  
-# 	'default': {     
-# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',       
-# 		'NAME': 'djangodb',       
-# 		'USER': 'djangouser',        
-# 		'PASSWORD': 'password',        
-# 		'HOST': 'localhost',       
-# 		'PORT': '',    
-# 	}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+DATABASES = {  
+	'default': {     
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',       
+		'NAME': 'djangodb',       
+		'USER': 'djangouser',        
+		'PASSWORD': 'password',        
+		'HOST': 'localhost',       
+		'PORT': '',    
+	}
+}
 
 
 # Password validation
